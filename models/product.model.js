bacf06bb35e39f8904d84575917fc1e6985a010f
Mohsanul786 <mdmohsanul@gmail.com>
2024-12-14 19:03:57 +0530

    bug: json object error resolved

--- models/product.model.js
+++ models/product.model.js
@@ -1,44 +1,44 @@
 const mongoose = require("mongoose");
 
-const TimberProductSchema = new mongoose.Schema({
-  products: [
-    {
-      name: {
-        type: String,
-      },
-      price: {
-        type: Number,
-      },
-      category: {
-        type: String,
-      },
-      rating: {
-        type: Number,
-      },
-      discount: {
-        type: Number,
-      },
-      timber_assured: {
-        type: Boolean,
-      },
-      brand_name: {
-        type: String,
-      },
-      image: {
-        type: String,
-      },
-      is_in_wishlist: {
-        type: Boolean,
-      },
-      is_in_cart: {
-        type: Boolean,
-      },
-      productId: {
-        type: String,
-      },
-    },
-  ],
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  category: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+  },
+  discount: {
+    type: Number,
+  },
+  timber_assured: {
+    type: Boolean,
+  },
+  brand_name: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  is_in_wishlist: {
+    type: Boolean,
+  },
+  is_in_cart: {
+    type: Boolean,
+  },
+  productId: {
+    type: String,
+  },
 });
 
-const Products = mongoose.model("Products", TimberProductSchema);
+// Define the main data schema with an array of products
+const dataSchema = new mongoose.Schema({
+  products: { type: [productSchema], required: true }, // Array of products
+});
+const Products = mongoose.model("Products", dataSchema);
 module.exports = Products;
