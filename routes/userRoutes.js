47de579ea2b25f73f7255654a85f26ddaebf79a4
Mohsanul786 <mdmohsan2407@gmail.com>
2025-01-22 17:55:26 +0530

    feat:added login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -46,7 +46,9 @@ router.post("/login", async (req, res) => {
       expiresIn: "1h",
     });
 
-    res.status(200).json({ message: "Login successful", token });
+    res
+      .status(200)
+      .json({ message: "Login successful", token, username: user.userName });
   } catch (error) {
     res.status(500).json({ message: "Error logging in", error });
   }
