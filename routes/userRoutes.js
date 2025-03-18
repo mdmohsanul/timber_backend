ab9e29f63a82ed4c07d4910e79ed644583f7961b
Mohsanul786 <mdmohsan2407@gmail.com>
2025-03-18 16:07:40 +0530

    fix:signup error handling

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,11 +13,11 @@ router.post("/signup", async (req, res) => {
 
   try {
     if (!userName || !email || !password) {
-      return res.status(400).json({ error: "Missing required fields" });
+      return res.status(400).json({ message: "Missing required fields" });
     }
     const existingUser = await User.findOne({ email });
     if (existingUser)
-      return res.status(400).json({ error: "User already exists" });
+      return res.status(400).json({ message: "User already exists" });
     // Hash or encrypt the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
